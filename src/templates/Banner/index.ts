329ae8c0afc4e6db3b158740513b14f154ff11ac
Stéphane <contact@stephane-richin.fr>
2020-07-01 15:49:13 +0200

    :twisted_rightwards_arrows: Merging develop

--- src/templates/Banner/index.ts
+++ src/templates/Banner/index.ts
@@ -1,20 +1,25 @@
-const Banner = () => `
-     <div id="banner-cookie" class="cookie-banner">
-          <p class="cookie-banner-text">
-              ${(window as any).tarteALaPralineLanguage.TEXT_BANNER}
-          </p>
-          <div class="cookie-banner-buttons-wrapper">
-               <button class="cookie-banner-button js-cookie-button-allow">${
-                 (window as any).tarteALaPralineLanguage.BUTTON_AGREE
-               }</button>
-               <button class="cookie-banner-button js-cookie-button-deny">${
-                 (window as any).tarteALaPralineLanguage.BUTTON_DECLINE
-               }</button>
-               <button class="cookie-banner-button js-cookie-button-customize" data-micromodal-trigger="js-cookie-modal">${
-                 (window as any).tarteALaPralineLanguage.BUTTON_CUSTOM
-               }</button>
-          </div>
+const Banner = (primaryColor: string): string => `
+  <style>
+    .cookie-banner-button {
+      color: ${primaryColor}
+    }
+  </style>
+  <div id="banner-cookie" class="cookie-banner" style="color: ${primaryColor};">
+    <p class="cookie-banner-text">
+      ${(window as any).tarteALaPralineLanguage.TEXT_BANNER}
+    </p>
+    <div class="cookie-banner-buttons-wrapper">
+      <button class="cookie-banner-button js-cookie-button-allow"><span>${
+        (window as any).tarteALaPralineLanguage.BUTTON_AGREE
+      }</span></button>
+      <button class="cookie-banner-button js-cookie-button-deny"><span>${
+        (window as any).tarteALaPralineLanguage.BUTTON_DECLINE
+      }</span></button>
+      <button class="cookie-banner-button js-cookie-button-customize" data-micromodal-trigger="js-cookie-modal"><span>${
+        (window as any).tarteALaPralineLanguage.BUTTON_CUSTOM
+      }</span></button>
     </div>
+  </div>
 `;
 
 export default Banner;
