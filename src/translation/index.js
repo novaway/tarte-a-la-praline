6e51aa66ff694e5e0d679434a69e826552455863
Stéphane <contact@stephane-richin.fr>
2020-07-01 10:01:40 +0200

    :sparkles: Adding custom translation support with default FR/EN module. Can add custom module on init function parameters

--- src/translation/index.js
+++
@@ -1,7 +0,0 @@
-export const TEXT_BANNER =
-  "En poursuivant votre navigation sur ce site, vous acceptez l'utilisation de cookies pour vous offrir une meilleure expérience utilisateur.";
-export const BUTTON_AGREE = "Accepter";
-export const BUTTON_DECLINE = "Refuser";
-export const BUTTON_CUSTOM = "Personnaliser";
-export const BUTTON_VALIDATION = "Valider";
-export const HEADER_MODAL = "Gestion des cookies";
