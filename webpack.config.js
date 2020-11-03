eae615df974769b526b3ae126b70d5ce20f95698
Stéphane <contact@stephane-richin.fr>
2020-11-03 14:58:17 +0100

    :wrench: Adding mini-css-extract-plugin webpack plugin and extract CSS files on build

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const config = {
-  entry: "./src/index.ts",
+  entry: {
+    app: "./src/index.ts",
+    cookieBanner: "./src/scss/cookie-banner.scss",
+    modal: "./src/scss/modal.scss"
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].js",
@@ -13,17 +18,25 @@ const config = {
   module: {
     rules: [
       {
+        test: /\.s?[ac]ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader', options: { url: false, sourceMap: true } },
+          { loader: 'sass-loader', options: { sourceMap: true } }
+        ]
+      },
+      {
         test: /\.ts(x)?$/,
         use: ["awesome-typescript-loader"],
         exclude: /node_modules/,
       },
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
     ],
   },
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      ignoreOrder: false
+    }),
     new HtmlWebpackPlugin({
       appMountId: "app",
       filename: "index.html",
