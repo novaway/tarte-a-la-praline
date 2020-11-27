6358555f5f23ec499d8a7e4075f3965f15e65f51
Stéphane <contact@stephane-richin.fr>
2020-11-27 09:23:58 +0100

    :globe_with_meridians: Adding German translation

---
+++ translations/de.js
@@ -0,0 +1,13 @@
+﻿const DE = {
+  TEXT_BANNER:
+    "Um Ihnen besser und persönlicher zu helfen, verwenden wir Cookies. Wenn Sie auf unserer Webseite weiter klicken, gehen wir davon aus, dass Sie damit einverstanden sind.",
+  BUTTON_AGREE: "Akzeptieren",
+  BUTTON_DECLINE: "Ablehnen",
+  BUTTON_CUSTOM: "Hier personalisieren",
+  BUTTON_VALIDATION: "Bestätigen",
+  HEADER_MODAL: "Cookie Einstellungen",
+  HEADER_MODAL_BUTTON_CLOSE: "Schließen"
+};
+
+window.TALP_SETTINGS = {};
+window.TALP_SETTINGS.language = DE;
