34e7679c0fdc01f74381da19c397b322320b8afb
Marc Balme <marc@novaway.fr>
2020-12-15 08:29:53 +0100

    :children_crossing: add div for content in banner

--- src/templates/Banner/index.ts
+++ src/templates/Banner/index.ts
@@ -8,6 +8,7 @@ const Banner = (primaryColor: string): string => {
       }
     </style>
     <div id="banner-cookie" class="${className.banner}" style="color: ${primaryColor};">
+      <div>
       <p class="${className.text}">
         ${language.TEXT_BANNER}
       </p>
@@ -15,6 +16,7 @@ const Banner = (primaryColor: string): string => {
         <button class="${className.button} js-cookie-button-allow"><span>${language.BUTTON_AGREE}</span></button>
         <button class="${className.button} js-cookie-button-deny"><span>${language.BUTTON_DECLINE}</span></button>
         <button class="${className.button} js-cookie-button-customize"><span>${language.BUTTON_CUSTOM}</span></button>
+      </div>
       </div>
     </div>
   `;
