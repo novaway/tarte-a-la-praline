840efbf38b58dc1d557f1716c2fb7272ef3fd80a
Chiara <chiara@novaway.fr>
2022-06-21 17:24:08 +0200

    fix : change if

--- src/utils/checkInput.ts
+++ src/utils/checkInput.ts
@@ -2,9 +2,9 @@ const checkInputWhenReOpenModal = () => {
     const oldServices = localStorage.getItem("oldServices");
 
     for (const [key, value] of Object.entries(JSON.parse(oldServices))) {
-        if (value == true){
+        if(key !== 'createdAt'){
             const checkbox = <HTMLInputElement>document.getElementById(key);
-            checkbox.checked = true;
+            checkbox.checked = Boolean(value);
         }
     }
 }
